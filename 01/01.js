2bbbf8de9a68a4df63bf07cb623b28d3eb6d8bfe
ali <ali@ali.com>
2024-02-06 17:05:57 +0330

    add file js -> type in js

---
+++ 01/01.js
@@ -0,0 +1,20 @@
+console.log("Hello word")
+
+let x=1
+console.log("x is type :", typeof x, "value x :" ,x)
+
+x="One"
+console.log("x is type :", typeof x, "value x :" ,x)
+
+x=true
+console.log("x is type :", typeof x, "value x :" ,x)
+
+x=[1,2,"A","B",false]
+console.log("x is type :", typeof x, "value x :" ,x)
+
+x={
+    key:123,
+    name:"Ali",
+    id:"@123"
+}
+console.log("x is type :", typeof x, "value x :" ,x)
