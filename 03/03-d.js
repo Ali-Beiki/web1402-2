e1321e9906e167a1945780f91eef84fa04b2613e
ali <ali@ali.com>
2024-02-20 16:57:55 +0330

    end edit 1

--- 03/03-d.js
+++ 03/03-d.js
@@ -4,14 +4,22 @@ let fs = require('fs');
 function callback(err){
     if (err){
 
-        if(err.code ==='EPERM'){//  folder
-            fs.rmdir(process.argv[2],callback)
+        if(err.code ==='EPERM'){// for folder
+            fs.rmdir(process.argv[2],rmCallback)
         }else{
             console.log("ERROR : ",err)
         }
         
     }else{
         console.log("File Delet !")
+    }
+}
+
+function rmCallback(err){
+    if (err){
+        console.log("ERROR : ",err)
+    }else{
+        console.log("Folder Delete !")
     }
 }
     fs.unlink(process.argv[2],callback)
