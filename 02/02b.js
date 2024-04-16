55d5ce8c11c1b18a3466dfc49acb44776a473af1
Your Name <you@example.com>
2024-04-16 18:34:20 +0330

    add

---
+++ 02/02b.js
@@ -0,0 +1,13 @@
+
+
+let j=0;
+ let array=[];
+let argv=process.argv;
+ for(let i=2;i<argv.length;i+=2){
+        array[j]={
+            item:process.argv[i],
+            item2:process.argv[i+1]
+        }
+        j++
+    };
+    console.log(array);
