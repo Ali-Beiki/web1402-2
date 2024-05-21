cee0b7c70680255f7a0386ee45b44d3b3015b793
Your Name <you@example.com>
2024-05-21 17:24:52 +0330

    Add

---
+++ 10/11.js
@@ -0,0 +1,102 @@
+let http = require('http');
+let port = 80;
+let server = http.createServer(requestHandler);
+server.listen(port);
+console.log("Server is running on port:" + port);
+
+let httpControllers = require('./httpControllers');
+let commandlineControllers = require('./commandlineControllers');
+let redisControllers = require('./redisControllers');
+const { log } = require('console');
+        
+function requestHandler(request, response){
+    console.log('request:', request.url);
+    command = request.url.split('/')[1];
+
+    // if(command === 'page1'){
+    //     response.writeHead(200, { 'Content-Type': 'text/plain'});
+    //     response.write('this is page1');
+    //     response.end();
+    // }
+    // if(command === 'page2'){
+    //     httpControllers.text(request, response);
+    // }
+    // if(command === 'page3'){
+    //     httpControllers.textFile(request, response);
+    // }
+    // if(command === 'create'){ 
+    //     httpControllers.createFile(request, response);
+    // }
+
+
+    let commands = {
+        'favicon.ico':function(){
+
+        },
+        page1: function(){
+            response.writeHead(200, { 'Content-Type': 'text/plain'});
+            response.write('this is page1');
+            response.end();
+        },
+        page2: function(){
+            httpControllers.text(request, response);
+        },
+        page3: function(){
+            httpControllers.textFile(request, response);
+        },
+        create: function(){
+            httpControllers.createFile(request, response);
+        },
+        copy: function(){
+            httpControllers.copyFile(request, response);
+        },
+        redisCreate: function(){
+            let name = request.url.split('/')[2];
+            let body = request.url.split('/')[3];
+            redisControllers.create(name, body);
+        },
+        page: function(){
+            httpControllers.htmlFile(request, response);
+        },
+        sum:function(){
+            httpControllers.sum(request, response);
+        },
+        zarb:function(){
+            httpControllers.zarb(request, response);
+        }
+    }
+
+    let data =[]
+
+    request.on('data',(part)=>{
+       data.push(part)
+    })
+    request.on('end',()=>{
+        
+        commands[command]();
+        console.log(data);
+    })
+    
+}
+
+if(process.argv[2]){
+    let command = process.argv[2];
+
+    let commands = {
+        page1: function(){
+            commandlineControllers.text();
+        },
+        create: function(){
+            let name = process.argv[3];
+            let body = process.argv[4];
+            commandlineControllers.create(name, body);
+        },
+        redisCreate: function(){
+            let name = process.argv[3];
+            let body = process.argv[4];
+            redisControllers.create(name, body);
+        }
+    }
+
+    commands[command]();
+}
